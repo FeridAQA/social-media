e883179aeabc8a06977457de7b11ccf7c222d882
ferid aqa <feridcodee@gmail.com>
2025-06-08 23:34:05 +0400

    message and 7 finshed

--- src/app/chat/chat.controller.ts
+++ src/app/chat/chat.controller.ts
@@ -1,23 +1,42 @@
 import {
   Body,
   Controller,
+  Get,
+  Param,
   Post,
+  Query,
   UseGuards
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthGard } from 'src/guards/auth.guard';
 import { ChatService } from './chat.service';
 import { SendMessageDto } from './dto/send-message.dto';
+import { GetChatMessagesDto } from './dto/get-chat-messages.dto';
 
 @Controller('chat')
 @ApiTags('Chat')
 @ApiBearerAuth()
 @UseGuards(AuthGard)
 export class ChatController {
-  constructor(private chatService: ChatService) {}
+  constructor(private chatService: ChatService) { }
+
+
+  @Get()
+  getChats() {
+    return this.chatService.getChats();
+  }
+
+  @Get(':chatId')
+  getChatMessages(
+    @Param('chatId') chatId: number,
+    @Query() body: GetChatMessagesDto,
+  ) {
+    return this.chatService.getChatMessages(chatId, body);
+  }
+
 
   @Post()
-  sendMessage(@Body() body:SendMessageDto){
+  sendMessage(@Body() body: SendMessageDto) {
     return this.chatService.sendMesssage(body)
   }
 }
--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -10,6 +10,8 @@ import { MessageEntity } from 'src/database/entities/Message.entity';
 import { User } from 'src/database/entities/User.entity';
 import { Repository } from 'typeorm';
 import { SendMessageDto } from './dto/send-message.dto';
+import { CHAT_LIST_SELECT, CHAT_MESSAGES_SELECT } from './chat.select';
+import { GetChatMessagesDto } from './dto/get-chat-messages.dto';
 // import { EventEmitter2 } from '@nestjs/event-emitter';
 // import { RedisService } from 'src/shared/libs/redis/redis.service';
 
@@ -24,6 +26,86 @@ export class ChatService {
     @InjectRepository(MessageEntity)
     private messageRepo: Repository<MessageEntity>,
   ) { }
+
+
+  async getChats() {
+    const myUser = await this.cls.get<User>('user')
+
+    let chats = await this.chatRepo
+      .createQueryBuilder('chat')
+      .select(CHAT_LIST_SELECT)
+      .leftJoin('chat.lastMessage', 'lastMessage')
+      .leftJoin('lastMessage.sender', 'sender')
+      .leftJoin('sender.profilePicture', 'senderProfilePicture')
+      .leftJoin('chat.participants', 'participants')
+      .leftJoin('participants.user', 'users')
+      .leftJoin('chat.participants', 'myParticipant')
+      .where(`myParticipant.userId=:userId`, { userId: myUser.id })
+      .getMany()
+
+    let result = chats.map((chat) => {
+      let myParticipant = chat.participants.find(
+        (p) => p.user.id === myUser.id,
+      );
+      return {
+        ...chat,
+        unreadCount: myParticipant.unreadCount,
+        everyoneRead: !chat.participants.find((p) => p.unreadCount > 0),
+        participants: undefined,
+      };
+    });
+
+    return result;
+  }
+
+  async getChatMessages(chatId: number, params: GetChatMessagesDto) {
+    const { limit = 10, page = 0 } = params;
+
+    const myUser = await this.cls.get<User>('user');
+
+    let chat = await this.chatRepo
+      .createQueryBuilder('chat')
+      .leftJoinAndSelect('chat.participants', 'participants')
+      .leftJoinAndSelect('participants.user', 'users')
+      .where('chat.id =:chatId', { chatId })
+      .getOne();
+
+    if (!chat || !chat.participants.find((p) => p.user.id === myUser.id))
+      throw new NotFoundException('Chat is not found');
+
+    let messages = await this.messageRepo.find({
+      where: {
+        chat: {
+          id: chat.id,
+        },
+      },
+      select: CHAT_MESSAGES_SELECT,
+      order: {
+        id: 'DESC',
+      },
+      take: limit,
+      skip: limit * page,
+      relations: ['sender'],
+    });
+
+    let myParticipant = chat.participants.find((p) => p.user.id === myUser.id);
+    myParticipant.unreadCount = 0;
+
+    messages.forEach((message) => {
+      if (!message.readBy.includes(myUser.id)) {
+        message.readBy.push(myUser.id);
+      }
+    });
+
+    await Promise.all([myParticipant.save(), this.messageRepo.save(messages)]);
+
+
+    return messages;
+  }
+
+
+
+
 
   async findOrCreateChat(params: { chatId?: number; userId?: number }) {
     let { chatId, userId } = params
@@ -79,14 +161,14 @@ export class ChatService {
         id: chat.id
       },
       message: params.messsage,
-      sender:{
-        id:myUser.id
+      sender: {
+        id: myUser.id
       },
       readBy: [myUser.id]
     })
     await message.save()
 
-    chat.lastMessage = {id:message.id}
+    chat.lastMessage = { id: message.id }
 
     chat.participants.map((participant) => {
       if (participant.user.id === myUser.id) return participant
@@ -98,7 +180,7 @@ export class ChatService {
     await chat.save()
 
     return {
-      status:true,
+      status: true,
       message
     }
   }
